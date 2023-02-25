2a9c9b466bc036d915c2c470c30a3ccab0f90105
Joaquin Labrador <86396736+joaquin-labrador@users.noreply.github.com>
2023-02-25 17:12:02 -0300

    Revert ""first commit""

--- api/src/model/Category.js
+++
@@ -1,8 +0,0 @@
-export class Category {
-  constructor({ id, name, updatedAt, createdAt }) {
-    this.id = id;
-    this.name = name;
-    this.updatedAt = updatedAt;
-    this.createdAt = createdAt;
-  }
-}
