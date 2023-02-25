6da5103548e94e67270a8c900a247bced15e30bd
Joaquin Labrador <86396736+joaquin-labrador@users.noreply.github.com>
2023-02-25 17:10:04 -0300

    Revert ""first commit""

--- api/src/container/loC.js
+++
@@ -1,9 +0,0 @@
-import { ArticleController } from "../controller/articleController.js";
-
-import { ArticleService } from "../service/articleService.js";
-
-import { ArticleRepository } from "../repository/articleRepostory.js";
-
-export const articleController = new ArticleController(
-  new ArticleService(new ArticleRepository())
-);
--- api/src/model/Thumbnail.js
+++
@@ -1,8 +0,0 @@
-export class Thumbnail {
-  constructor({ id, url, updatedAt, createdAt }) {
-    this.id = id;
-    this.url = url;
-    this.updatedAt = updatedAt;
-    this.createdAt = createdAt;
-  }
-}
