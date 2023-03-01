d32f1a0a61070f618fedf5062885c612de59d50a
joaquin-labrador <joaquinlabrador1010@gmail.com>
2023-02-28 23:38:07 -0300

    add🆕: news unit test for request of categories

--- api/test/test.js
+++ api/test/test.js
@@ -68,7 +68,6 @@ describe("GET /api/wiki-ideas/getPagedArticles ", () => {
       .expect(200)
       .expect("Content-Type", /json/)
       .then((res) => {
-        console.log(res._body);
         assert.isNotEmpty(res._body);
         res.body.forEach((article) => {
           assert.containsAllKeys(article, [
@@ -81,6 +80,77 @@ describe("GET /api/wiki-ideas/getPagedArticles ", () => {
             "UpdatedAt",
             "id_category",
             "id_thumbnail",
+          ]);
+        });
+      })
+      .then(
+        () => done(),
+        (err) => done(err)
+      );
+  });
+});
+
+describe("GET /api/wiki-ideas/getCategories ", () => {
+  it("should return all categories", (done) => {
+    request(app)
+      .get("/api/wiki-ideas/getCategories")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        res._body.forEach((category) => {
+          assert.isNotEmpty(res._body);
+          assert.containsAllKeys(category, [
+            "id",
+            "name",
+            "UpdatedAt",
+            "CreatedAt",
+          ]);
+        });
+      })
+      .then(
+        () => done(),
+        (err) => done(err)
+      );
+  });
+
+  describe("GET /api/wiki-ideas/getCategory/id ", () => {
+    it("should return category with id", (done) => {
+      request(app)
+        .get("/api/wiki-ideas/getCategory/1")
+        .expect(200)
+        .expect("Content-Type", /json/)
+        .then((res) => {
+          assert.equal(res._body.id, 1);
+          assert.isNotEmpty(res._body);
+          assert.containsAllKeys(res._body, [
+            "id",
+            "name",
+            "UpdatedAt",
+            "CreatedAt",
+          ]);
+        })
+        .then(
+          () => done(),
+          (err) => done(err)
+        );
+    });
+  });
+});
+
+describe("GET /api/wiki-ideas/getPagedCategories ", () => {
+  it("should return a range of category", (done) => {
+    request(app)
+      .get("/api/wiki-ideas/getPagedCategories/?take=4&skip=3")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        assert.isNotEmpty(res._body);
+        res.body.forEach((category) => {
+          assert.containsAllKeys(category, [
+            "id",
+            "name",
+            "UpdatedAt",
+            "CreatedAt",
           ]);
         });
       })
